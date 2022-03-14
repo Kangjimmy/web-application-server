d6a727eb9e5b7cef99389a1c5501a4e50f86799f
Kangjimmy <50311129+Kangjimmy@users.noreply.github.com>
2022-03-14 21:25:15 +0900

    3.1index.html응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,13 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,15 +22,37 @@ public class RequestHandler extends Thread {
     }
 
     public void run() {
-        log.debug("New Client Connect!~~! Connected IP : {}, Port : {}", connection.getInetAddress(),
+        log.debug("New Client Connect!! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        try (OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        	String st = "";
+        	String url = "";
+        	boolean isUrl = false;
+        	while ((st = br.readLine()) != null) {
+        		if (st.equals("")) {
+        			break;
+        		}
+        		url = st.split(" ")[1];
+        		
+        		System.out.println(url);
+        		if (url.equals("/index.html")) {
+        			isUrl = true;
+        			break;
+        		}
+        	}
+        	String fileName = "./webapp";
+        	byte[] bytes = "Hello World".getBytes();
+        	if (isUrl) {
+        		bytes = Files.readAllBytes(Paths.get(fileName + url));  
+        	}
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            
+            response200Header(dos, bytes.length);
+            responseBody(dos, bytes);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
